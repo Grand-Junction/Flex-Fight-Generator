e04e05ccde9b73de07dc5973b04ae72b28d38c32
Grand-Junction <godsgrace414@gmail.com>
2025-04-14 18:54:05 -0500

    2nd commit: create & style Welcome component

---
+++ flex-fight-gen/src/Welcome.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Welcome() {
+    return (
+    <div id="welcome-div">
+        <h1>Welcome to Dear Days 2!</h1>
+        <p>QUOTE COMPONENT HERE</p>
+        BUTTON COMPONENT HERE
+    </div>
+    );
+}
+
+export default Welcome;
