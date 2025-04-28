bfba221be51d87ea425be155edd6bf1afbb8ff7b
Grand-Junction <godsgrace414@gmail.com>
2025-04-28 18:37:01 -0500

    finish Footer component

--- flex-fight-gen/src/Footer.jsx
+++ flex-fight-gen/src/Footer.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 
-// add dynamic date for current year, flesh out copyright message
+const date = new Date().getFullYear();
 
 function Footer() {
     return (
         <footer>
             <ul className="flex-parent-2">
-                <li className="flex-child" id="footer-copyright">Copyright © </li>
+                <li className="flex-child" id="footer-copyright">Copyright © {date} ZIEGFRIEDT</li>
             </ul>
         </footer>
     );
