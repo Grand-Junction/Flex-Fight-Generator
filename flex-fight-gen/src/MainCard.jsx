947c24589dc36768167a5b8c0dbe46f644c01fbf
Grand-Junction <godsgrace414@gmail.com>
2025-05-01 20:40:55 -0500

    create Fight Card component

--- flex-fight-gen/src/MainCard.jsx
+++ flex-fight-gen/src/MainCard.jsx
@@ -3,7 +3,14 @@ import React from "react";
 function MainCard() {
     return (
         <div className="main-card">
-            <h1>MAIN CARD</h1>
+            <div className="fight-card">
+                <div className="card-top">
+                    <img src="https://picsum.photos/200/300" alt="" />
+                </div>
+                <div className="card-bottom">
+
+                </div>
+            </div>
         </div>
     );
 }
