1b65651cc826cc2787f36db71b2f19aa34bc1f32
Grand-Junction <godsgrace414@gmail.com>
2025-05-01 19:26:38 -0500

    create MainCard component

--- flex-fight-gen/src/HomePage.jsx
+++ flex-fight-gen/src/HomePage.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
+import Card from "./MainCard";
 
 function HomePage() {
     return (
         <div>
             <Header />
+            <Card />
             <Footer />
         </div>
 
---
+++ flex-fight-gen/src/MainCard.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function MainCard() {
+    return (
+        <div className="main-card">
+            <h1>MAIN CARD</h1>
+        </div>
+    );
+}
+
+export default MainCard;
