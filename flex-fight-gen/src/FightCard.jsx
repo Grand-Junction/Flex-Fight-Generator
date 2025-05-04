d708ad63765d563cbf9c8821d491e2bc100b223a
Grand-Junction <godsgrace414@gmail.com>
2025-05-04 11:08:03 -0500

    clean up Fight component, create Fight-Btn component

--- flex-fight-gen/src/FightCard.jsx
+++ flex-fight-gen/src/FightCard.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 
 function FightCard() {
-    return ;
+    return (
+        <div className="main-card">
+        <div className="fight-card">
+            <h2>RANDOM OPPONENT:</h2>
+            <img src="https://picsum.photos/275/400" alt="" />
+            <button type="button" className="fight-btn">FIGHT!</button>
+        </div>
+        <div className="fight-card">
+            <h2 id="nation">SELECT NATION:</h2>
+            <img src="https://picsum.photos/275/400" alt="" />
+            <button type="button" className="fight-btn">FIGHT!</button>
+        </div>
+    </div>
+    ) ;
 }
 
 export default FightCard;
--- flex-fight-gen/src/MainCard.jsx
+++ flex-fight-gen/src/MainCard.jsx
@@ -1,20 +1,9 @@
 import React from "react";
-import Button from "./Button";
+import FightCard from "./FightCard";
 
 function MainCard() {
     return (
-        <div className="main-card">
-            <div className="fight-card">
-                <h2>RANDOM OPPONENT:</h2>
-                <img src="https://picsum.photos/300/400" alt="" />
-                <Button />
-            </div>
-            <div className="fight-card">
-                <h2 id="nation">SELECT NATION OPPONENT:</h2>
-                <img src="https://picsum.photos/300/400" alt="" />
-                <Button />
-            </div>
-        </div>
+        <FightCard />
     );
 }
 
