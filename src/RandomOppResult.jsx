a4fe74d9991ef8fb42c6b8b9b32adf750823412e
Grand-Junction <godsgrace414@gmail.com>
2025-05-20 19:17:52 -0500

    update RandomOppResult and SelectNationResult components

--- src/RandomOppResult.jsx
+++ src/RandomOppResult.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
+import Button from "./Button";
 
 function RandomOppResult() {
     return(
-        <div>
-            <h1>Random Opp Result!</h1>
+        <div id="fight-main-div">
+            <div className="fight-card">
+                <h2>RANDOM RESULT HERE</h2>
+                <img src="https://picsum.photos/275/400" alt="" />
+                <div className="results-btn">
+                    <span><Button btnText="NEW RANDOM OPPONENT"/></span>
+                    <span><Button btnText="RETURN TO FIGHT PAGE"/></span>
+                </div>
+            </div>
         </div>
     );
 }
--- src/SelectNationResult.jsx
+++ src/SelectNationResult.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 
 function SelectNationResult() {
-    return <h1>Select Nation Result!</h1>;
+    return(
+                <div id="fight-main-div">
+            <div className="fight-card">
+                <h2>SELECT RESULT HERE</h2>
+                <img src="https://picsum.photos/275/400" alt="" />
+                <div className="results-btn">
+                    <span><Button btnText="NEW SE OPPONENT"/></span>
+                    <span><Button btnText="RETURN TO FIGHT PAGE"/></span>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default SelectNationResult;
