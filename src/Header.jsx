a7e67093203dcab5435032e61c3067c1670bcf05
Grand-Junction <godsgrace414@gmail.com>
2025-05-25 10:32:55 -0500

    add route to Header return home button

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import React from "react";
 import Button from "./Button";
 
@@ -7,7 +8,7 @@ function Header() {
             <nav>
                 <ul id="header-parent">
                     <li className="header-child" id="app-title">Dear Days Fight Generator</li>
-                    <span id="header-btn"><Button btnText="RETURN HOME" /></span>
+                    <span id="header-btn"><Link to="/"><Button btnText="RETURN HOME" /></Link></span>
                 </ul>
             </nav>
         </header>
