4976eeca0422879f40c0f60f9e7cfa599e66f798
Grand-Junction <godsgrace414@gmail.com>
2025-05-26 13:02:22 -0500

    added border to welcome component btn

--- src/Welcome.jsx
+++ src/Welcome.jsx
@@ -8,7 +8,9 @@ function Welcome() {
         <div className="welcome-div-2">
             <h1 id="welcome-child-1">Welcome to Dear Days 2!</h1>
             <h2>Prepare to test your mettle! No lose intended!</h2>
-            <Link to="/homepage"><Button btnText="ENTER HERE" /></Link>
+            <div className="results-btn">
+                <Link to="/homepage"><Button btnText="ENTER HERE" /></Link>
+            </div>
         </div>
     </div>
     );
