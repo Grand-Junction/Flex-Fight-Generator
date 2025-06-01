594c99a1756b60e83659032ef6c5d8bfb68b9487
Grand-Junction <godsgrace414@gmail.com>
2025-05-31 21:34:14 -0500

    update Fighters object w/ main boss names

--- src/Fighters.js
+++ src/Fighters.js
@@ -4,234 +4,273 @@ const Fighters = [
     id: 1,
     fighter: 'Yuki Ichidoji',
     nation: 'Keter Sanctuary',
+    main: 'Sword Saint Knight Dragon Emperor, Gramgrace Sieg',
     level: [ 'Hard', 'Very Hard' ]
   },
   {
     id: 2,
     fighter: 'Rasen Ichidoji',
     nation: 'Stoicheia',
+    main: 'A Viridescent Flash, Rorowa',
     level: [ 'Easy', 'Normal', 'Hard', 'Very Hard' ]
   },
   {
     id: 3,
     fighter: 'Gosetsu Katsushika',
     nation: 'Stoicheia',
+    main: 'Flagship Dragon, Flagburg Dragon',
     level: [ 'Easy', 'Normal', 'Hard', 'Very Hard' ]
   },
   {
     id: 4,
     fighter: 'Kanji Meguro',
     nation: 'Dark States',
+    main: 'Purgatory Dragon Deity, Favrneel',
     level: [ 'Easy', 'Normal', 'Hard', 'Very Hard' ]
   },
   {
     id: 5,
     fighter: 'Tatsuki Bunkyo',
     nation: 'Dark States',
+    main: 'Soul Oratorio, Kheios',
     level: [ 'Easy', 'Normal', 'Hard', 'Very Hard' ]
   },
   {
     id: 6,
     fighter: 'Masaru Kita',
     nation: 'Dark States',
+    main: 'Master of Gravity, Baromagnes',
     level: [ 'Easy', 'Normal', 'Hard', 'Very Hard' ]
   },
   {
     id: 7,
     fighter: 'Danji Momoyama',
     nation: 'Dark States',
+    main: 'Diabolos, "Viamance" Bruce',
     level: [ 'Hard', 'Very Hard' ]
   },
   {
     id: 8,
     fighter: 'Suo Yobitsugi',
     nation: 'Dark States',
+    main: 'Fated One of Zero, Blangdmire',
     level: [ 'Hard', 'Very Hard' ]
   },
   {
     id: 9,
     fighter: 'Michiru Hazama',
     nation: 'Dark States',
+    main: 'Demonic Jewel True Dragon, Drajeweled Ignis',
     level: [ 'Very Hard' ]
   },
   {
     id: 10,
     fighter: 'Haru Shinigawa',
     nation: 'Keter Sanctuary',
+    main: 'One Who Walks the Path of Light, Thegrea',
     level: [ 'Normal', 'Hard', 'Very Hard' ]
   },
   {
     id: 11,
     fighter: 'Kuro Minato',
     nation: 'Keter Sanctuary',
+    main: 'Hexaorb Sorceress',
     level: [ 'Easy', 'Normal', 'Hard', 'Very Hard' ]
   },
   {
     id: 12,
     fighter: 'Kisato Kuga',
     nation: 'Keter Sanctuary',
+    main: 'Majesty Lord Blaster',
     level: [ 'Normal', 'Hard', 'Very Hard' ]
   },
   {
     id: 13,
     fighter: 'Gaku Nijo',
     nation: 'Keter Sanctuary',
+    main: 'Dragheart, Luard',
     level: [ 'Normal', 'Hard', 'Very Hard' ]
   },
   {
     id: 14,
     fighter: 'Raika Koshiba',
     nation: 'Keter Sanctuary',
+    main: 'Youthberk "RevolForm: Full Blast"',
     level: [ 'Hard', 'Very Hard' ]
   },
   {
     id: 15,
     fighter: 'Akina Myodo',
     nation: 'Keter Sanctuary',
+    main: 'Fated One of Miracles, Rezael',
     level: [ 'Hard', 'Very Hard' ]
   },
   {
     id: 16,
     fighter: 'Hikari Myodo',
     nation: 'Keter Sanctuary',
+    main: 'Fated One of Time, Liael=Amorta',
     level: [ 'Very Hard' ]
   },
   {
     id: 17,
     fighter: 'Megumi Okura',
     nation: 'Stoicheia',
+    main: 'Sylvan Horned Beast Emperor, Magnolia Elder',
     level: [ 'Very Hard' ]
   },
   {
     id: 18,
     fighter: 'Urara Haneyama',
     nation: 'Stoicheia',
+    main: 'Marche Fleur, Lianorn Vivace',
     level: [ 'Hard', 'Very Hard' ]
   },
   {
     id: 19,
     fighter: 'Masenori Iseki',
     nation: 'Stoicheia',
+    main: 'Fated One of Taboo, Zorga Nadir',
     level: [ 'Hard', 'Very Hard' ]
   },
   {
     id: 20,
     fighter: 'Mika Shibuya',
     nation: 'Lyrical Monasterio',
+    main: 'Astesice-Live, Kairi',
     level: [ 'Easy', 'Normal', 'Hard', 'Very Hard' ]
   },
   {
     id: 21,
     fighter: 'Kana Aoyama',
     nation: 'Lyrical Monasterio',
+    main: 'MiMish, Fortia',
     level: [ 'Normal', 'Hard', 'Very Hard' ]
   },
   {
     id: 22,
     fighter: 'Yui Itabashi',
     nation: 'Lyrical Monasterio',
+    main: 'Heartfelt Song, Loronerol',
     level: [ 'Normal', 'Hard', 'Very Hard' ]
   },
   {
     id: 23,
     fighter: 'Mikoto Saito',
     nation: 'Lyrical Monasterio',
+    main: 'Fated One of Ever-changing, Krysrain',
     level: [ 'Hard', 'Very Hard' ]
   },
   {
     id: 24,
     fighter: 'Doshi Nakano',
     nation: 'Dragon Empire',
+    main: 'Heavy Artillery of Dust Storm, Eugene',
     level: [ 'Easy', 'Normal', 'Hard', 'Very Hard' ]
   },
   {
     id: 27,
     fighter: 'Noi Taito',
     nation: 'Dragon Empire',
+    main: 'Will-o-wisp Daybreak, Tamayura',
     level: [ 'Easy', 'Normal', 'Hard', 'Very Hard' ]
   },
   {
     id: 28,
     fighter: 'Otawa Dosaki',
     nation: 'Dragon Empire',
+    main: 'Hellfire Dragon, Wirbel Dragon',
     level: [ 'Hard', 'Very Hard' ]
   },
   {
     id: 29,
     fighter: 'Roh Nijo',
     nation: 'Dragon Empire',
+    main: 'Demon Stealth Dragon, Shiranui "Oboro"',
     level: [ 'Very Hard' ]
   },
   {
     id: 30,
     fighter: 'Yu-yu Kondo',
     nation: 'Dragon Empire',
+    main: 'Chakrabarthi Phoenix Dragon, Nirvana Jheva',
     level: [ 'Very Hard' ]
   },
   {
     id: 31,
     fighter: 'Mirei Minae',
     nation: 'Dragon Empire',
+    main: 'Sealed Blaze Maiden, Bavsargra',
     level: [ 'Hard', 'Very Hard' ]
   },
   {
     id: 32,
     fighter: 'Nao Inabe',
     nation: 'Dragon Empire',
+    main: 'Fated One of Unparalleled, Varga Dragres',
     level: [ 'Hard', 'Very Hard' ]
   },
   {
     id: 33,
     fighter: 'Zero Tendo',
     nation: 'Dragon Empire',
+    main: 'Rewrite the Star, Vyrgilla',
     level: [ 'Very Hard' ]
   },
   {
     id: 34,
     fighter: 'Taiji Arakawa',
     nation: 'Brandt Gate',
+    main: 'Fountain of Knowledge, Eva',
     level: [ 'Easy', 'Normal', 'Hard', 'Very Hard' ]
   },
   {
     id: 35,
     fighter: 'Zakusa Ishigame',
     nation: 'Brandt Gate',
+    main: 'Cardinal Dominus, Orfist Regis',
     level: [ 'Hard', 'Very Hard' ]
   },
   {
     id: 36,
     fighter: 'Tomari Seto',
     nation: 'Brandt Gate',
+    main: 'Aurora Fierce Princess, Seraph Purelight',
     level: [ 'Hard', 'Very Hard' ]
   },
   {
     id: 37,
     fighter: 'Taizo Kiyokura',
     nation: 'Brandt Gate',
+    main: 'Fated One of Guiding Star, Welstra "Blitz Arms"',
     level: [ 'Hard', 'Very Hard' ]
   },
   {
     id: 38,
     fighter: 'Jinki Mukae',
     nation: 'Keter Sanctuary',
+    main: 'Destined One of Protection, Alden',
     level: [ 'Very Hard' ]
   },
   {
     id: 39,
     fighter: 'Senka Kanae',
     nation: 'Brandt Gate',
+    main: 'Destined One of Exceedance, Impauldio',
     level: [ 'Very Hard' ]
   },
   {
     id: 40,
     fighter: 'Kyohma Kurosaki',
     nation: 'Dragon Empire',
+    main: 'Destined One of Scales, Aelquilibra',
     level: [ 'Very Hard' ]
   },
   {
     id: 41,
     fighter: 'Yuna Saionji',
     nation: 'Lyrical Monasterio',
+    main: 'Destined One of Supremacy, Lisciafael',
     level: [ 'Very Hard' ]
   }
 ];
