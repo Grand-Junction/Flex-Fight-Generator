5f77d96b9474ca9daf4308d0eb819b16303d4844
Grand-Junction <godsgrace414@gmail.com>
2025-06-21 14:28:18 -0500

    update welcome page h1

--- src/Welcome.jsx
+++ src/Welcome.jsx
@@ -6,8 +6,11 @@ function Welcome() {
     return (
     <div id="welcome-div">
         <div className="welcome-div-2">
-            <h1 id="welcome-child-1">Welcome to Dear Days 2!</h1>
-            <h2>Prepare to test your mettle! No lose intended!</h2>
+            <h1 id="welcome-child-1">
+                Dear Days <br />
+                Battleground
+            </h1>
+            <h2>Stand up, Vanguard!</h2>
             <div className="results-btn">
                 <Link to="/homepage"><Button btnText="ENTER HERE" /></Link>
             </div>
