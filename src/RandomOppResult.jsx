5adeadd9aace6b656633a1f46471e2d26d17bbd5
Grand-Junction <godsgrace414@gmail.com>
2025-07-15 19:47:07 -0500

    updated fighters array with set 5 fighters & card images

--- src/RandomOppResult.jsx
+++ src/RandomOppResult.jsx
@@ -1,36 +1,43 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 import Button from "./Button";
 import Fighters from "./Fighters";
 import RenderedFighterRandom from "./RenderedFighterRandom";
 
 function RandomOppResult() {
-
-    function randomRendered() {
-        
-        const randomFighter = Math.floor(Math.random() * Fighters.length);
-        const randomFighterID = Fighters[randomFighter].id;
-        const randomFighterName = Fighters[randomFighter].fighter;
-        const randomFighterMain = Fighters[randomFighter].main;
-        const randomFighterLevel = Fighters[randomFighter].level[Math.floor(Math.random() * Fighters[randomFighter].level.length)];
-
-        return <RenderedFighterRandom 
-        key={randomFighterID}
-        selectedFighter={randomFighterName} 
-        selectedMain={randomFighterMain}
-        selectedLevel={randomFighterLevel}
-        />
-    }
+  function randomRendered() {
+    const randomFighter = Math.floor(Math.random() * Fighters.length);
+    const randomFighterID = Fighters[randomFighter].id;
+    const randomFighterCard = Fighters[randomFighter].card;
+    const randomFighterName = Fighters[randomFighter].fighter;
+    const randomFighterMain = Fighters[randomFighter].main;
+    const randomFighterLevel =
+      Fighters[randomFighter].level[
+        Math.floor(Math.random() * Fighters[randomFighter].level.length)
+      ];
 
     return (
-        <div id="fight-main-div">
-            <div className="fight-card">
-                {randomRendered()}
-                <div className="results-btn">
-                    <Link to='/homepage'><Button btnText="RETURN TO FIGHT PAGE"/></Link>
-                </div>
-            </div>
-        </div>
+      <RenderedFighterRandom
+        key={randomFighterID}
+        selectedImg={randomFighterCard}
+        selectedFighter={randomFighterName}
+        selectedMain={randomFighterMain}
+        selectedLevel={randomFighterLevel}
+      />
     );
+  }
+
+  return (
+    <div id="fight-main-div">
+      <div className="fight-card">
+        {randomRendered()}
+        <div onClick="" className="results-btn">
+          <Link to="/homepage">
+            <Button btnText="RETURN TO FIGHT PAGE" />
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default RandomOppResult;
--- src/RenderedFighterRandom.jsx
+++ src/RenderedFighterRandom.jsx
@@ -1,14 +1,14 @@
-
 function RenderedFighterRandom(props) {
-    return (
-        <div className="rendered-fighter-random">
-            <h3>Your Opponent For This Fight:</h3>
-            <h2 className="random-selected-fighter">{props.selectedFighter}</h2>
-            <em><h3 id="bossTxt">{props.selectedMain}</h3></em>
-            <h3 id="difficulty">Difficulty Level:</h3>
-            <h2 className="random-selected-fighter">{props.selectedLevel}</h2>
-        </div>
-    );
+  return (
+    <div className="rendered-fighter-random">
+      <img className="fighter-img-bg" src={props.selectedImg} alt="" />
+      <h3>Your Opponent For This Fight:</h3>
+      <h2 className="random-selected-fighter">{props.selectedFighter}</h2>
+      <em>
+        <h3 id="bossTxt">{props.selectedMain}</h3>
+      </em>
+    </div>
+  );
 }
 
 export default RenderedFighterRandom;
