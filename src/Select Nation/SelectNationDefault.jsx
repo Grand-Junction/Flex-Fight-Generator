76c86dc0041f03d7a2ee4f03bfbbb5998ba0a483
Grand-Junction <godsgrace414@gmail.com>
2025-08-05 17:53:57 -0500

    update Random Opponent components to arrow funcs

--- src/Select Nation/SelectNationDefault.jsx
+++ src/Select Nation/SelectNationDefault.jsx
@@ -1,13 +1,15 @@
-import { Link } from 'react-router-dom';
-import Button from "../Components/Button"
+import { Link } from "react-router-dom";
+import Button from "../Components/Button";
 
 function SelectNationDefault() {
-    return (
-        <>
-            <h2 id="nation">SELECT NATION:</h2>
-            <Link to="/select"><Button btnText="TIME TO FIGHT!"/></Link>
-        </>
-    );
+  return (
+    <div>
+      <h2 id="nation">SELECT NATION:</h2>
+      <Link to="/select">
+        <Button btnText="TIME TO FIGHT!" />
+      </Link>
+    </div>
+  );
 }
 
 export default SelectNationDefault;
--- src/Select Nation/SelectNationPage.jsx
+++ src/Select Nation/SelectNationPage.jsx
@@ -4,15 +4,13 @@ import Footer from "../Components/Footer";
 import MainCardSelectNation from "../Main Card/MainCardSelectNation";
 
 function SelectNationPage() {
-
-    return (
-        <div id="homepage-parent-div">
-            <Header />
-                <MainCardSelectNation />
-            <Footer />
-        </div>
-    );
- 
+  return (
+    <div id="homepage-parent-div">
+      <Header />
+      <MainCardSelectNation />
+      <Footer />
+    </div>
+  );
 }
 
 export default SelectNationPage;
